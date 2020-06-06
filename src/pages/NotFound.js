5feef0f1b51ac8e8db00e5cfb97703399055051d
Alexandra Moore <alexmoore94@hotmail.co.uk>
2020-06-06 18:41:51 +0100

    404 page designed

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from 'styled-components';
 
+import { ReactComponent as NotFoundImg } from '../assets/404.svg';
+
 function NotFound() {
     return (
         <NotFoundContainer>
+            <NotFoundImg />
             <h3>Sorry, page not found!</h3>
         </NotFoundContainer>
     );
@@ -12,6 +15,10 @@ function NotFound() {
 const NotFoundContainer = styled.div`
     display: flex;
     justify-content: center;
+    flex-direction: column;
+    height: 60vh;
+    margin-top: 10vh;
+    font-size: 1.5rem;
 `;
 
 export default NotFound;
